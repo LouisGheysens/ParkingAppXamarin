bd4179b1ad198bce4f2bb1b1eadfd4bebe5892b5
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-05-29 22:12:13 +0200

    Init

---
+++ ParkingApp/ParkingApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ParkingApp.ViewModels;
+using ParkingApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ParkingApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ ParkingApp/ParkingApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ParkingApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ParkingApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
